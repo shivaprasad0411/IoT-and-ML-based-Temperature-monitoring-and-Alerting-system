b1015cd4bc6fd154856d610d66344f6f544de911
Naikawadi Shivaprasad <92375412+shivaprasad0411@users.noreply.github.com>
2022-06-07 17:37:04 +0530

    Added configuration file containing credentials

---
+++ conf.py
@@ -0,0 +1,8 @@
+SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"            # replace with your Twilio account SID
+AUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'    # replace with your Twilio account AUTH_TOKEN
+FROM_NUMBER = '+9999999999'                         # replace with your Twilio account trial number
+TO_NUMBER = '+91999999999'                          # replace with the mobile number on which you want to recieve SMS
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'           # replace with you bolt API key
+DEVICE_ID = 'BOLTXXXXX'                             # replace with your bolt device id
+MUL_FACTOR = 10     # you can change these value to control theresholds
+FRAME_SIZE = 10     # you can change frame size to control number of data points you need
